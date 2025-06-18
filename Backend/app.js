954add6422d7224240c9fb7062979335e34ef399
Ishan Saini <ishansaini0105@gmail.com>
2025-06-18 11:52:01 +0530

    Frontend url in backend

--- Backend/app.js
+++ Backend/app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://hermies-frontend.onrender.com", // Replace with your frontend URL
     credentials: true,
   })
 );
